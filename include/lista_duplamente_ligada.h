b2901bf4986645ef1efd98bd9ed4be3f53d14abe
Mateus Pepice <mateuspepice@hotmail.com>
2023-04-06 20:06:16 -0300

    Arquivos alterados

---
+++ include/lista_duplamente_ligada.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h> 
+
+typedef struct no No;
+typedef struct cabeca cabeca;
+
+void adicionar_inicio():
+void adicionar_final():
+
+void remover_inicio():
+void remover_final():
+
+void buscar_inicio():
+void buscar_final():
+
+void imprimir_inicio_final();
+void imprimir_final_inicio();
+
+void ordenar_quick():
--- src/lista_duplamente_ligada.c
+++ src/lista_duplamente_ligada.c
@@ -4,3 +4,37 @@
 #include <stdbool.h>
 
 //A partir daqui, basta começar os códigos, Structs e funções.
+struct no
+{
+    int valor;
+    No *ptr_proximo;
+    No *ptr_anterior;
+};
+
+struct cabeca
+{
+    int tamanho;
+    No *ptr_cabeca;
+    No *ptr_cauda;
+};
+
+cabeca *criar_lista() {
+
+   cabeca *prt_lista = (cabeca*) malloc(sizeof(cabeca));
+   prt_lista->tamanho = 0;
+   prt_lista->ptr_cabeca = NULL;
+   prt_lista->ptr_cauda = NULL;
+ 
+   return  prt_lista;
+
+}
+
+void adicionar_final(cabeca **ptr_auxiliar, int valor){
+    
+}
+void remover_final(cabeca **ptr_auxiliar){
+
+}
+void ordenar(){
+
+}
