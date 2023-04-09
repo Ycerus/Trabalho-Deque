bdc1d981ee77c21b8d50c9cb67284d61b2aa6fc9
Tom <wellingtontopanoot@gmail.com>
2023-04-09 12:20:37 -0300

    Alterações

---
+++ include/lista_duplamente_ligada.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <stdbool.h> 
+
+typedef struct no No;
+
+typedef struct lista Lista;
+
+typedef struct no No;
+
+typedef struct lista Lista;
+
+Lista *iniciar_lista();
--- src/lista_duplamente_ligada.c
+++ src/lista_duplamente_ligada.c
@@ -3,4 +3,36 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//A partir daqui, basta começar os códigos, Structs e funções.
+
+struct aluno
+{
+    int ra;
+    char nome[50];
+    int idade;
+} Aluno;
+
+
+struct no
+{
+    struct aluno aluno;
+    struct No *ptr_prox;
+    struct No *ptr_ant;
+};
+
+struct lista
+{
+    int tamanho;
+    No *ptr_cabeca;
+    No *ptr_cauda;
+};
+
+
+Lista *iniciar_lista(){
+    Lista *l = (Lista*)malloc(sizeof(Lista));
+    l->tamanho = 0;
+    l->ptr_cabeca = NULL;
+    l->ptr_cauda = NULL;
+
+    return l;
+}
+
